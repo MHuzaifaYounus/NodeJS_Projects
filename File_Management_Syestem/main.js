323af04135ebaa96213dab2dfc423ca29686c3d5
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-08-30 09:10:16 +0500

    fix bugs

--- File_Management_Syestem/main.js
+++ File_Management_Syestem/main.js
@@ -1,47 +1,32 @@
-const express = require("express")
-const fs = require("fs")
-const path = require("path")
+#! /usr/bin/env node
+import express from "express"
+import fs from "fs"
+import fsp from "fs/promises"
+import inquirer from "inquirer"
+import path from "path"
 const app = express()
 const port = 3000
 
-let items = fs.readdirSync(__dirname + "/files")
+let input = await inquirer.prompt({
+   type: "input",
+   name: "dir",
+   message: "Enter The Directory of your Folder: ",
+})
+let dir = input.dir
+
+let items = await fsp.readdir(dir)
+
 items.forEach(element => {
-    if (path.extname(element) === ".png") {
-        if (!fs.existsSync("files/PNG")) {
-            fs.mkdirSync("files/PNG");
-        }
-        fs.renameSync(__dirname + "/files/" + element, __dirname + "/files/PNG/" + element);
-    }
-    else if (path.extname(element) === ".jpg") {
-        if (!fs.existsSync("files/JPG")) {
-            fs.mkdirSync("files/JPG");
-        }
-        fs.renameSync(__dirname + "/files/" + element, __dirname + "/files/JPG/" + element);
-    }
-    else if (path.extname(element) === ".pdf") {
-        if (!fs.existsSync("files/PDF")) {
-            fs.mkdirSync("files/PDF");
-        }
-        fs.renameSync(__dirname + "/files/" + element, __dirname + "/files/PDF/" + element);
-    }
-    else if (path.extname(element) === ".mp3") {
-        if (!fs.existsSync("files/MP3")) {
-            fs.mkdirSync("files/MP3");
-        }
-        fs.renameSync(__dirname + "/files/" + element, __dirname + "/files/MP3/" + element);
-    }
-    else if (path.extname(element) === ".svg") {
-        if (!fs.existsSync("files/SVG")) {
-            fs.mkdirSync("files/SVG");
-        }
-        fs.renameSync(__dirname + "/files/" + element, __dirname + "/files/SVG/" + element);
-    }
-    else {
-        if (!fs.existsSync("files/Others")) {
-            fs.mkdirSync("files/Others");
-        }
-        fs.renameSync(__dirname + "/files/" + element, __dirname + "/files/Others/" + element);
-    }
+   if(element.includes(".")){
+      let ext =  path.extname(element).split(".")[1]
+      if(fs.existsSync(dir+"/"+ext)){
+         fsp.rename(dir+"/"+element,dir+"/"+ext+"/"+element)
+      }
+      else if(!fs.existsSync(dir+"/"+ext)){
+         fsp.mkdir(dir+"/"+ext)
+         fsp.rename(dir+"/"+element,dir+"/"+ext+"/"+element)
+      }  
+   }
 });
 
 app.listen(port)
